399571bf17c5b385d65a0bf41c2efa23069aafe1
SriharshaMandru <mandrusriharsha@gmail.com>
2024-06-17 13:24:27 +0530

    modifications are made

--- network.js
+++ network.js
@@ -4,16 +4,16 @@ function myFunction() {
         document.querySelector('p').innerHTML = "Internet is connected";
         document.querySelector('.icon').classList.remove('offline');
         document.querySelector('.box').classList.remove('offline');
-        document.querySelector('.icon i').className = 'uil uil-wifi'; // Resetting the icon class
+        document.querySelector('.icon i').className = 'uil uil-wifi';
     } else {
         document.querySelector('span').innerHTML = 'You are offline now';
         document.querySelector('p').innerHTML = "Internet is Disconnected";
         document.querySelector('.icon').classList.add('offline');
         document.querySelector('.box').classList.add('offline');
-        document.querySelector('.icon i').className = 'uil uil-wifi-slash'; // Changing to offline icon
+        document.querySelector('.icon i').className = 'uil uil-wifi-slash'; 
     }
 }
 
 setInterval(() => {
     myFunction();
-}, 500); // Increased interval to 500ms for better performance
+}, 500); 
